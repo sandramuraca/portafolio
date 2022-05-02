0af7663493a51e246f7ceb45880dee51ac181068
Sandra Muraca <sandramuraca78@gmail.com>
2022-05-02 20:56:21 -0300

    correccion archivo php

--- enviar.php
+++ enviar.php
@@ -5,15 +5,15 @@ $email = $_POST['email'];
 $telefono = $_POST['telefono'];
 $mensaje = $_POST['mensaje'];
 
-$header = 'From: ' . $email . "\r\n";
-$header.= "X-Mailer: PHP/"  . phpversion() . "\r\n";
+$header = 'From: ' . $email . " \r\n";
+$header.= "X-Mailer: PHP/"  . phpversion() . " \r\n";
 $header.= "Mime-Version: 1.0 \r\n";
 $hrader.= "Content_Type: text/plain";
 
-$mensaje = "Este mensjae fue enviado por: " . $nombre . "\r\n";
+$mensaje = "Este mensjae fue enviado por: " . $nombre . " \r\n";
 $mensaje .= "Su email es: " . $email. "\r\n";
-$mensaje .= "Su telefono de contacto es: " . $telefono ."\r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . "\r\n";
+$mensaje .= "Su telefono de contacto es: " . $telefono ." \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 
 $para = "sandramuraca78@gmail.com";
 $asunto = "Mensaje desde portfolio";
