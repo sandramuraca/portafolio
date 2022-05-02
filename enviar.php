f6d47bce6eaf196b4a3b324a381b243378cf7582
Sandra Muraca <sandramuraca78@gmail.com>
2022-05-02 20:28:33 -0300

    agrego funcionalidad de enviar formulario

---
+++ enviar.php
@@ -0,0 +1,24 @@
+<?php
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' . $email . "\r\n";
+$header.= "X-Mailer: PHP/"  . phpversion() . "\r\n";
+$header.= "Mime-Version: 1.0 \r\n";
+$hrader.= "Content_Type: text/plain";
+
+$mensaje = "Este mensjae fue enviado por: " . $nombre . "\r\n";
+$mensaje .= "Su email es: " . $email. "\r\n";
+$mensaje .= "Su telefono de contacto es: " . $telefono ."\r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . "\r\n";
+
+$para = "sandramuraca78@gmail.com";
+$asunto = "Mensaje desde portfolio";
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:index.html");
+?>
